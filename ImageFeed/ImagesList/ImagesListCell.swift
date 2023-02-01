599f61c631b6645fabe483d586f09c1206445606
AlekseiKryzhanovskii <mr.cooper37@mail.ru>
2023-02-01 14:52:49 +0300

    1. Создан новый проект 2. Свёрстан Launch Screen приложения 3. Свёрстан главный экран с лентой изображений

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Алексей on 28.01.2023.
+//
+
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+
+    
+    @IBOutlet weak var imageInCell: UIImageView!
+   
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    
+}
